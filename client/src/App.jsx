ff93b1b6a74a3a7871e8f61906a4918e040fa033
JaimeGSandoval <jaime.devs@gmail.com>
2022-10-25 00:29:08 -0700

    set up client side dir and css resets

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import './sass/App.scss';
+import { Test } from './components/styleTest';
+
+function App() {
+  return (
+    <div className='App'>
+      <Test />
+      {/* <div className='container grid grid-max grid-bg grid-height'>
+        <div className='col-12 bg-crimson'></div>
+        <div className='col-2 bg-green'></div>
+        <div className='col-1 bg-blue'></div>
+        <div className='col-3 bg-yellow'></div>
+        <div className='col-12 bg-pink'></div>
+      </div> */}
+    </div>
+  );
+}
+
+export default App;
