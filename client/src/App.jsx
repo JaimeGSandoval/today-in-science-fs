8aa8d6424ae3de016775cbda13a5f46c9f2e44c5
JaimeGSandoval <jaime.devs@gmail.com>
2022-10-27 23:06:31 -0700

    add router links for login and signup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,16 @@
+import { Routes, Route, Outlet, Link } from 'react-router-dom';
 import { Header } from './components/Header';
+import Signup from './views/Signup';
+import Login from './views/Login';
 
 function App() {
   return (
     <div className='App'>
       <Header />
+      <Routes>
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
     </div>
   );
 }
