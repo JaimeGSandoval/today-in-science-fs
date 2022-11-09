3c58af02732c6ce19e4b8850561b98c9701c29d4
JaimeGSandoval <jaime.devs@gmail.com>
2022-11-09 14:16:36 -0800

    add signup & login buttons to mobileNav component

--- client/src/components/MobileNav/MobileNavMenu.jsx
+++ client/src/components/MobileNav/MobileNavMenu.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { createPortal } from 'react-dom';
 import NavLink from '../NavLink';
 import styles from './_mobileNav.module.scss';
@@ -19,10 +20,27 @@ export const MobileNavMenu = ({ isOpen, setIsOpen }) => {
   return createPortal(
     <div id='navbarModal' className={`${styles.modalContainer} ${isOpen ? styles.modalOpen : ''}`}>
       <div className={styles.innerContainer}>
-        <div className={styles.mobileClose} onClick={() => setIsOpen(false)}>
-          &#x2715;
-        </div>
-        <span className={styles.logo}>today in science</span>
+        <header className={styles.header}>
+          <ul className={styles.authBtnsBox}>
+            <li>
+              <Link to='/signup' className={styles.authBtn} onClick={() => setIsOpen(false)}>
+                Sign Up
+              </Link>
+            </li>
+            <span className={styles.pipe}>|</span>
+            <li>
+              <Link to='/login' className={styles.authBtn} onClick={() => setIsOpen(false)}>
+                Login
+              </Link>
+            </li>
+          </ul>
+          <div className={styles.mobileClose} onClick={() => setIsOpen(false)}>
+            &#x2715;
+          </div>
+        </header>
+        <Link to='/' className={styles.logo}>
+          today in science
+        </Link>
         <nav className={styles.mobileNav}>
           <ul className={styles.subjectsList}>
             {SUBJECTS.map((subject) => (
