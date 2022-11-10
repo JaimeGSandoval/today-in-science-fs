f5951ea0585c8acb45932834622f990229d35098
JaimeGSandoval <jaime.devs@gmail.com>
2022-11-09 18:44:41 -0800

    Add state for controlled form inputs
    - add state changes for inout change and styles to inform user if
    their input has met the requirements

--- client/src/views/AuthViews/SignupView.jsx
+++ client/src/views/AuthViews/SignupView.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import { FaUser, FaLock, FaKey } from 'react-icons/fa';
 import { MdEmail } from 'react-icons/md';
@@ -6,6 +6,82 @@ import styles from './_forms.module.scss';
 import { HeaderLogo } from '../../components/HeaderLogo';
 
 export const SignupView = () => {
+  const [username, setUsername] = useState('');
+  const [validUsername, setValidUsername] = useState(false);
+  const usernameRef = useRef();
+
+  const [email, setEmail] = useState('');
+  const [validEmail, setValidEmail] = useState(false);
+  const emailRef = useRef();
+
+  const [password, setPassword] = useState('');
+  const [validPassword, setValidPassword] = useState(false);
+  const passwordRef = useRef();
+
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [validConfirm, setValidConfirm] = useState(false);
+  const confirmRef = useRef();
+
+  const [success, setSuccess] = useState(false);
+
+  const addValidOutline = (refVal) => {
+    refVal.current.classList.add(styles.validField);
+    refVal.current.classList.remove(styles.invalidField);
+  };
+
+  const removeValidOutline = (refVal) => {
+    refVal.current.classList.add(styles.invalidField);
+    refVal.current.classList.remove(styles.validField);
+  };
+
+  useEffect(() => {
+    if (username.length < 6) {
+      removeValidOutline(usernameRef);
+      setValidUsername(false);
+    } else {
+      addValidOutline(usernameRef);
+      setValidUsername(true);
+    }
+  }, [username]);
+
+  useEffect(() => {
+    if (email.length < 6) {
+      removeValidOutline(emailRef);
+      setValidEmail(false);
+    } else {
+      addValidOutline(emailRef);
+      setValidEmail(true);
+    }
+  }, [email]);
+
+  useEffect(() => {
+    if (password.length < 6) {
+      removeValidOutline(passwordRef);
+      setValidPassword(false);
+    } else {
+      addValidOutline(passwordRef);
+      setValidPassword(true);
+    }
+  }, [password]);
+
+  useEffect(() => {
+    if (confirmPassword.length < 6) {
+      removeValidOutline(confirmRef);
+      setValidConfirm(false);
+    } else {
+      addValidOutline(confirmRef);
+      setValidConfirm(true);
+    }
+  }, [confirmPassword]);
+
+  useEffect(() => {
+    if (validUsername && validEmail && validPassword && validConfirm) {
+      setSuccess(true);
+    } else {
+      setSuccess(false);
+    }
+  }, [validUsername, validEmail, validPassword, validConfirm]);
+
   return (
     <section className={styles.container}>
       <HeaderLogo />
@@ -20,15 +96,18 @@ export const SignupView = () => {
               </label>
               <FaUser className={styles.icon} />
               <input
+                id='username'
                 className={styles.field}
                 type='text'
-                id='username'
                 placeholder='Username (6 - 20) characters'
                 required
                 aria-required
                 tabIndex={0}
                 minLength={6}
                 maxLength={20}
+                onChange={(e) => setUsername(e.target.value)}
+                value={username}
+                ref={usernameRef}
               />
             </div>
 
@@ -38,13 +117,16 @@ export const SignupView = () => {
               </label>
               <MdEmail className={styles.icon} />
               <input
+                id='email'
                 className={styles.field}
                 type='email'
-                id='email'
                 placeholder='Email'
                 required
                 aria-required
                 tabIndex={0}
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+                ref={emailRef}
               />
             </div>
 
@@ -54,14 +136,17 @@ export const SignupView = () => {
               </label>
               <FaLock className={styles.icon} />
               <input
+                id='password'
                 className={styles.field}
                 type='password'
-                id='password'
                 placeholder='Password (min 6 characters)'
                 minLength='6'
                 required
                 aria-required
                 tabIndex={0}
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+                ref={passwordRef}
               />
             </div>
 
@@ -71,13 +156,16 @@ export const SignupView = () => {
               </label>
               <FaKey className={styles.icon} />
               <input
+                id='confirmPassword'
                 className={styles.field}
                 type='password'
-                id='confirmPassword'
                 placeholder='Confirm password'
                 required
                 aria-required
                 tabIndex={0}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                value={confirmPassword}
+                ref={confirmRef}
               />
             </div>
 
@@ -90,7 +178,11 @@ export const SignupView = () => {
               </small>
             </div>
 
-            <button className={styles.signupSubmitBtn} tabIndex={0}>
+            <button
+              className={success ? styles.signupSubmitBtn : styles.disabledSubmitBtn}
+              tabIndex={0}
+              disabled={success ? false : true}
+            >
               Sign Up
             </button>
           </div>
