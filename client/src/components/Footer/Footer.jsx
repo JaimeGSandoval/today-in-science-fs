d30a102c8c36bd821c687664ac8ee89babae83e6
JaimeGSandoval <jaime.devs@gmail.com>
2022-12-22 18:53:01 -0800

    remove categories from footer & replace with resources

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import NavLink from '../NavLink';
+import FooterLink from './FooterLink';
 import styles from './_footer.module.scss';
 
 const SUBJECTS_ONE = [
@@ -12,24 +12,62 @@ const SUBJECTS_ONE = [
 
 const SUBJECTS_TWO = ['Mathematics', 'Neural Interfaces', 'Neuroscience', 'Quantum Computers'];
 
+const RESOURCES_ONE = [
+  {
+    url: 'https://www.space.com/',
+    name: 'SPACE.COM',
+  },
+  {
+    url: 'https://phys.org/',
+    name: 'PHYS.ORG',
+  },
+  {
+    url: 'https://www.sciencedaily.com/',
+    name: 'SCIENCEDAILY.COM',
+  },
+  {
+    url: 'https://www.livescience.com/',
+    name: 'LIVESCIENCE.COM',
+  },
+];
+
+const RESOURCES_TWO = [
+  {
+    url: 'https://www.spacex.com/',
+    name: 'SPACEX.COM',
+  },
+  {
+    url: 'https://www.scientificamerican.com/',
+    name: 'SCIENTIFICAMERICAN.COM',
+  },
+  {
+    url: 'https://www.popsci.com/',
+    name: 'POPSCI.COM',
+  },
+  {
+    url: 'https://www.nasa.gov/',
+    name: 'NASA.GOV',
+  },
+];
+
 export const Footer = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.container}>
-        <h1 className={styles.headline}>Categories</h1>
+        <h1 className={styles.headline}>More Resources</h1>
         <nav className={styles.nav}>
           <div className={styles.linkBox}>
             <ul>
-              {SUBJECTS_ONE.map((subject) => (
-                <NavLink subject={subject} styles={styles.navLink} key={subject} />
+              {RESOURCES_ONE.map((resource) => (
+                <FooterLink resource={resource} styles={styles.navLink} key={resource.name} />
               ))}
             </ul>
           </div>
 
           <div className={styles.linkBox}>
             <ul>
-              {SUBJECTS_TWO.map((subject) => (
-                <NavLink subject={subject} styles={styles.navLink} key={subject} />
+              {RESOURCES_TWO.map((resource) => (
+                <FooterLink resource={resource} styles={styles.navLink} key={resource.name} />
               ))}
             </ul>
           </div>
