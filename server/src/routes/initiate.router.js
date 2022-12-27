ca08bc581acc63c44a4897cc42a7c3a335f632ff
JaimeGSandoval <jaime.devs@gmail.com>
2022-12-27 13:17:17 -0800

    change name of rssRouter to initiateRouter

--- server/src/routes/initiate.router.js
+++ server/src/routes/initiate.router.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const rssController = require('../controllers/rss.controller');
+// const rssController = require('../controllers/rss.controller');
 const initiateController = require('../controllers/initiate.controller');
 
 const router = express.Router();
-router.get('/', rssController.httpRssFeed);
+// router.get('/', rssController.httpRssFeed);
 router.get('/initiate', initiateController.httpInitiate);
 
 module.exports = router;
