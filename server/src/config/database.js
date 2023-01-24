bbe6a36c9afc4db80258054da41f67a0a0735a27
JaimeGSandoval <jaime.devs@gmail.com>
2023-01-24 15:55:06 -0800

    add user to .env for PG

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -25,6 +25,7 @@ dotenv.config({ path: path.join(__dirname, '..', '..', '..', '/.env') });
 const db = new Pool({
   connectionString: process.env.DATABASE_URL,
   password: process.env.PG_PASSWORD,
+  user: process.env.PG_AWS_USER,
 });
 
 module.exports = db;
